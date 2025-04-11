70160d980aab2a5f32b0adbb04d2b1b286edd951
Poicalee <115011843+Poicalee@users.noreply.github.com>
2025-04-11 02:04:34 +0200

    Operators

---
+++ Operators.py
@@ -0,0 +1,24 @@
+﻿from xml.etree.ElementTree import XMLParser
+
+
+def operators(name):
+    a = 10
+    h = 5
+    
+    area = 0.5 * h * a
+    print(area)
+
+    x = 15
+    y = 4
+    
+    print(x//y)
+    print(x%y)
+    print(x**y)
+
+    wiek = 19
+    warunek = (wiek >= 18) and (wiek < 65)
+    
+    print(warunek) 
+
+if __name__ == '__main__':
+    operators('PyCharm')
