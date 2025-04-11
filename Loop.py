fcd7cdb9174d4e38edd8d3821799ae5ae374ffa3
Poicalee <115011843+Poicalee@users.noreply.github.com>
2025-04-11 02:32:25 +0200

    Create Loop.py

---
+++ Loop.py
@@ -0,0 +1,29 @@
+﻿
+def Loop(name):
+    
+    sum = 0
+    
+    for i in range(1,101):
+        sum += i
+       
+    print(sum)
+    
+    i = 0
+    power = 2
+    while(power < 1000):
+        i += 1
+        power = 2**i
+    
+    print(i)
+
+    liczby = [12, 5, 8, 21, 13, 7]
+    najwieksza = 0
+    
+    for liczba in liczby:
+        if liczba > najwieksza:
+            najwieksza = liczba
+    
+    print(najwieksza)
+
+if __name__ == "__main__":
+    Loop("PyCharm")
