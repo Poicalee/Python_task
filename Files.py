b5cbc970cc7217314a5a2e30c9505b67d302a1de
Poicalee <115011843+Poicalee@users.noreply.github.com>
2025-04-16 02:50:32 +0200

    Created Files.py
    Some files tasks

---
+++ Files.py
@@ -0,0 +1,41 @@
+﻿#Zadanie 1
+def files():
+    with open('przyklady.txt','w',encoding='utf8') as f:
+        f.write("To jest pierwsza linia\n")
+        f.write("To jest druga linia\n")
+        f.write("To jest trzecia linia")
+    
+    with open('przyklady.txt','r',encoding='utf8') as f:
+         zawartosc = f.read()
+         print(zawartosc)
+
+#Zadanie 2
+def copy_file():
+    try:
+     with open('przyklady.txt','r',encoding='utf8') as f:
+        zawartosc = f.read()
+        with open('przyklady2.txt','w',encoding='utf8') as f:
+            f.write(zawartosc)
+            print(zawartosc)
+    except FileNotFoundError:
+        print(f"Plik nie istniej")
+        return 0
+
+#Zadanie 3        
+def ask_for_file():
+    try:
+        file_name = input("Podaj nazwę pliku: ")
+        with open(file_name,'r',encoding='utf8') as f:
+            zawartosc = f.read()
+            slowa = zawartosc.split()
+            print(zawartosc)
+            print(len(slowa))
+            return len(slowa)
+    except FileNotFoundError:
+        print(f"Plik nie istniej")
+        
+    
+    
+# files()
+# copy_file()
+ask_for_file()
